2a05a8c72ca41f94d85273fb0d72e3c88418fe2d
Falconiere R. Barbosa <falconieer@gmail.com>
2024-02-05 21:28:57 -0300

    fix build

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-const BASE_URL = typeof window !== "undefined" ? window.location.origin : 'http://localhost:3000';
+import { NEXT_PUBLIC_APP_URL } from "@/constants/constants"
+
+const BASE_URL = NEXT_PUBLIC_APP_URL
 const http = {
   get:async (url:string) => {
     return await fetch(`${BASE_URL}${url}`, {
